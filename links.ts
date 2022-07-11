00888dd5f191a581fdf25f529c8c820287b1cd2f
Yoni Kosiner <yoni@kosiner.co.uk>
2022-07-11 15:24:41 +0100

    stuff

--- links.ts
+++ links.ts
@@ -7,5 +7,25 @@ export const links: link[] = [
     {
         "name": "Instagram",
         "url": "https://instagram.com/kosiner.codes"
+    },
+    {
+        "name": "Twitter",
+        "url": "https://twitter.com/KosinerYoni"
+    },
+    {
+        "name": "YouTube",
+        "url": "https://youtube.com/c/yonikosiner"
+    },
+    {
+        "name": "Latest YouTubeu video",
+        "url": ""
+    },
+    {
+        "name": "Github",
+        "url": "https://github.com/yonikosiner"
+    },
+    {
+        "name": ".dotfiles",
+        "url": "https://github.com/yonikosiner/.dotfiles"
     }
 ]
