623b912c1eb24e1215f554f0b7dd91c333c89ef3
Yoni Kosiner <yoni@kosiner.co.uk>
2022-07-11 15:20:17 +0100

    work?

---
+++ links.ts
@@ -0,0 +1,11 @@
+export interface link {
+    name: string,
+    url: string,
+}
+
+export const links: link[] = [
+    {
+        "name": "Instagram",
+        "url": "https://instagram.com/kosiner.codes"
+    }
+]
